6072ac402ca3002b27f4a3c484414fd1975dff25
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2018-07-26 16:50:17 +0900

    Design up swipe button

--- src/components/commons/task-row.js
+++ src/components/commons/task-row.js
@@ -2,7 +2,7 @@
 
 import React from 'react'
 
-import { View, Text, TouchableHighlight, TouchableOpacity } from 'react-native'
+import { View, TouchableOpacity } from 'react-native'
 import Swipeable from 'react-native-swipeable'
 import styled from 'styled-components'
 import Ionicons from 'react-native-vector-icons/Ionicons'
@@ -42,16 +42,47 @@ const Description = styled.Text`
   margin: 2px;
 `
 
+const SwipedButton = styled.TouchableHighlight`
+  height: 100%;
+  background-color: ${props => props.color};
+  padding: 0 10px;
+`
+
+const SwipeButtonInnerView = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+const SwipeButtonInnerText = styled.Text`
+  color: white;
+  font-size: 16px;
+`
+
 export const TaskRow = (props: Props) => {
   const { task, toggleTask, deleteTask } = props
 
   const rightButtons = [
-    <TouchableHighlight key={ '1' }>
-      <Text>{'お願い！'}</Text>
-    </TouchableHighlight>,
-    <TouchableHighlight key={ '2' } onPress={ deleteTask }>
-      <Text>{'削除'}</Text>
-    </TouchableHighlight>,
+    <SwipedButton key={ '1' } color={ 'red' }>
+      <SwipeButtonInnerView>
+        <Ionicons
+          name={ 'ios-notifications' }
+          size={ 20 }
+          style={ { color: 'white', padding: 2 } }
+        />
+        <SwipeButtonInnerText>{'お願い！'}</SwipeButtonInnerText>
+      </SwipeButtonInnerView>
+    </SwipedButton>,
+    <SwipedButton key={ '2' } color={ 'red' } onPress={ deleteTask }>
+      <SwipeButtonInnerView>
+        <Ionicons
+          name={ 'ios-trash' }
+          size={ 20 }
+          style={ { color: 'white', padding: 2 } }
+        />
+        <SwipeButtonInnerText>{'削除'}</SwipeButtonInnerText>
+      </SwipeButtonInnerView>
+    </SwipedButton>,
   ]
 
   return (
