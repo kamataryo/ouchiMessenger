ee978d65bf0c3632957e57e8d56e8fb19025b7e2
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2018-07-31 13:01:11 +0900

    Fix token onRegister handler

--- src/components/system/notification-handler.js
+++ src/components/system/notification-handler.js
@@ -31,9 +31,9 @@ export class NotificationHandler extends React.Component<Props> {
    */
   componentDidMount() {
     PushNotification.configure({
-      onRegister: deviceToken =>
+      onRegister: ({ token }) =>
         // NOTE: Did it called with Emulator?
-        this.props.updateDeviceToken(deviceToken || DUMMY_ACCESS_TOKEN),
+        this.props.updateDeviceToken(token || DUMMY_ACCESS_TOKEN),
 
       onNotification: notification => {
         const currentBadgeNumber = this.props.notifications.length
